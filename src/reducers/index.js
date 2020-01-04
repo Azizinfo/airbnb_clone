6fab99970cb48ed6feeb0bb5e87e10db29fd184c
aziz <aziz.icheddadene@gmail.com>
2020-01-04 14:50:05 +0100

    adds login and register,protected router, add auth service

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,11 +1,15 @@
 import thunk from 'redux-thunk';
 import { rentalReducer ,selectedRentalReducer } from './rental-reducer';
+import { authReducer } from './auth-reducer';
 import { createStore, applyMiddleware, compose ,combineReducers} from 'redux';
+import { reducer as formReducer } from 'redux-form';
 
 export const init = () =>{
     const reducer = combineReducers({
         rentals : rentalReducer,
-        rental : selectedRentalReducer
+        rental : selectedRentalReducer,
+        form : formReducer,
+        auth : authReducer
     })
     
     const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
