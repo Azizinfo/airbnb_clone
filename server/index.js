7a0e738a350ab4efe277407632a9d4b6c38637f1
aziz <aziz.icheddadene@gmail.com>
2020-01-30 22:32:15 +0100

    fix mongoo base

--- server/index.js
+++ server/index.js
@@ -27,8 +27,7 @@ app.use('/api/v1/bookings',bookingRoutes);
 
 if(process.env.NODE_ENV ==="production"){
     const appPath = path.join(__dirname, '..', 'build');
-    app.use(express.static(appPath));
-    
+    app.use(express.static(appPath));    
     app.get('*', function(req, res){
        res.sendFile(path.resolve(appPath, 'index.html'));
     })
