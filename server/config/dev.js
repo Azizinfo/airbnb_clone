7ee1f356619fe076661d342b017decd56ea47c71
Azizinfo <aziz.icheddadene@gmail.com>
2020-02-22 22:08:11 +0100

    Update dev.js

--- server/config/dev.js
+++ server/config/dev.js
@@ -1,7 +1,5 @@
 module.exports = {
-     DB_URL : 'mongodb+srv://testuser:testuser@cluster0-gljdq.mongodb.net/airbnb-database?retryWrites=true&w=majority',
-     SECRET : 'azertyuiop'
-     //DB_URL : 'mongodb+srv://test:testest@airbnb-dev-data-rz1ni.mongodb.net',
-     //SECRET : 'azdfghjuuytreza'
+     DB_URL : '****************************',
+     SECRET : '****************************'
 }
 
